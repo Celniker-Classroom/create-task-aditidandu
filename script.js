cb6e721750e59ac17b5c391dcc656f72ba57c1db
aditidandu <70790422+aditidandu@users.noreply.github.com>
2026-04-17 02:59:33 +0000

    added the questions for quiz, creating the arrays. Also made the buttons to check for answers

--- script.js
+++ script.js
@@ -1,68 +1,32 @@
-let tasks = [];
+let questions = [
 
-function addTask(){
-    let name = document.getElementById("task").value;
-    let priority = document.getElementById("priority").value; 
-    let deadline = document.getElementById("deadline").value;
-    
-    let task = {
-        name: name,
-        priority: priority,
-        deadline: deadline,
-        completed: false
-    }
+    "What is the most commonly spoken language?",
+    "What is the largest national park in the US by area?",
+    "Which country's national animal is a unicorn?",
+    "What is the most used currency in the world?",
+    "Where is the Hagia Sophia located?",
+    "Which country spans the most time zones?",
+    "What is the most populated city in the world?", 
+    "Which country had three capital cities?",
+    "Which ancient civilization built Machu Picchu?",
+    "Which of the following is not one of the US presidents featured on Mount Rushmore?", 
+    "What castle is the Disney castle based on?",
 
-    tasks.push(task);
-    displayTasks();
-}
+];
 
+let answers = [
+    ["Hindi", "English", "Spanish", "Chinese(Mandarin)"],
+    ["Gates of the Arctic", "Wrangell-St. Elias", "Death Valley", "Lake Clark"],
+    ["Wales", "Scotland", "Norway", "Finland"],
+    ["US dollar", "Euro", "Chinese yuan", "British pound"],
+    ["Poland", "Italy", "Turkey", "Greece"],
+    ["Russia", "USA", "China", "France"],
+    ["Tokyo", "Dhaka", "Shanghai", "Jakarta"],
+    ["Bolivia", "South Africa", "Italy", "Australia"],
+    ["Mayans", "Zapotecs", "Incas", "Aztecs"],
+    ["Abraham Lincoln", "James Madison", "Theordore Roosevelt", "Thomas Jefferson"],
+    ["Neuschwanstein Castle", "Windsor Castle", "Château de Chambord", "Prague Castle"],
 
-function toggleComplete(index){
-    tasks[index].completed = !tasks[index].completed;
-    displayTasks();
-}
+];
 
-
-function displayTasks(){
-    let list = document.getElementById("taskList");
-    let output = "";
-
-    tasks.sort(function(a, b){
-        let priorityOrder = {high:1, medium:2, low:3};
-        if (priorityOrder[a.priority] !== priorityOrder[b.priority]) {
-            return priorityOrder[a.priority] - priorityOrder[b.priority];
-        } else {
-            return new Date(a.deadline) - new Date(b.deadline);
-        }
-
-    });
-
-
-    for (let i = 0; i < tasks.length; i++){
-
-        let highlights = "";
-        if (tasks[i].priority === "high"){ 
-            highlights = "high";
-        }else if (tasks[i].priority === "medium"){ 
-            highlights = "medium";
-        }if (tasks[i].priority === "low"){ 
-            highlights = "low";
-        }
-
-        if (tasks[i].completed === true) {
-            output += "<s>";
-        }
-        
-        output += "<span class='" + highlights + "'>" + tasks[i].name + " (" + tasks[i].priority + ")" + " - Due: " + tasks[i].deadline + "</span>";
-        
-        if (tasks[i].completed === true) {
-            output += "</s>";
-        }
-        
-        output += " <button onclick='toggleComplete(" + i + ")'>checkmark</button>";
-        output += "<br>";
-    }
-    
-
-    list.innerHTML = output;
-}
+let rightAnswers = [3, 1, 1, 0, 2, 3, 1, 2, 1, 0];
