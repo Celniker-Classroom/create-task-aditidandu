cc63f7472af5b062f3b621a3459d3511de2238a4
aditidandu <70790422+aditidandu@users.noreply.github.com>
2026-04-21 20:09:52 +0000

    I made a starting messaged, hide buttons and text before and after pressing the start button in js, formatted the buttons and titles in css.

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ let currentQuestion = 0;
 let playerScore = 0;
 
 let questions = [
-    "What is the most commonly spoken language?",
-    "What is the largest national park in the US by area?",
-    "Which country's national animal is a unicorn?",
-    "What is the most used currency in the world?",
-    "Where is the Hagia Sophia located?",
-    "Which country spans the most time zones?",
-    "What is the most populated city in the world?", 
-    "Which country had three capital cities?",
-    "Which ancient civilization built Machu Picchu?",
-    "Which of the following is not one of the US presidents featured on Mount Rushmore?", 
-    "What castle is the Disney castle based on?",
+    "1. What is the most commonly spoken language?",
+    "2. What is the largest national park in the US by area?",
+    "3. Which country's national animal is a unicorn?",
+    "4. What is the most used currency in the world?",
+    "5. Where is the Hagia Sophia located?",
+    "6. Which country spans the most time zones?",
+    "7. What is the most populated city in the world?", 
+    "8. Which country had three capital cities?",
+    "9. Which ancient civilization built Machu Picchu?",
+    "10. Which of the following is not one of the US presidents featured on Mount Rushmore?", 
+    "11. What castle is the Disney castle based on?",
 ];
 
 let answers = [
@@ -31,8 +31,25 @@ let answers = [
 
 let correctAnswers = [3, 1, 1, 0, 2, 3, 3, 1, 2, 1, 0];
 
+document.getElementById("question").hidden = true;
+document.getElementById("btn0").hidden = true;
+document.getElementById("btn1").hidden = true;
+document.getElementById("btn2").hidden = true;
+document.getElementById("btn3").hidden = true;
+document.getElementById("nxtBtn").hidden = true;
+
 document.getElementById("startbtn").addEventListener("click", function() {
+
     document.getElementById("startbtn").hidden = true;
+
+    document.getElementById("question").hidden = false;
+    document.getElementById("btn0").hidden = false;
+    document.getElementById("btn1").hidden = false;
+    document.getElementById("btn2").hidden = false;
+    document.getElementById("btn3").hidden = false;
+    document.getElementById("nxtBtn").hidden = false;
+    document.getElementById("startingMsg").hidden = true;
+
     displayQuestion();
 });
 
